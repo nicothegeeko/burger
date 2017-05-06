7dac38e220b26598fee3ae81cce1d3cae44ae905
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-05-05 20:47:29 -0700

    adding all my burger database goodies

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model to use its database functions.
+var burgers = require("../models/burgers.js");
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burgers.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burgers.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, false
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.update({
+    devoured: true
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
